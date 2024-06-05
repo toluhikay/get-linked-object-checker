3f8240e77ae63308aa28c5876ca366a6daf0eb22
toluhikay <kayodesunday29@gmail.com>
2024-06-05 12:12:14 +0100

    route handler updated

--- app/api/brightness/route.ts
+++ app/api/brightness/route.ts
@@ -3,7 +3,6 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
   try {
-    // Get current brightness
     const currentBrightness = await brightness.get();
     return NextResponse.json({ messsage: currentBrightness });
   } catch (error) {
